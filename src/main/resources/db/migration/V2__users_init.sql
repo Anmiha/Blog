1d1ae159d8d75e8132511af1030429d495be8810
anmiha0672@mail.ru <anmiha0672@mail.ru>
2021-04-12 11:02:32 +0500

    Added migration users

---
+++ src/main/resources/db/migration/V2__users_init.sql
@@ -0,0 +1,19 @@
+INSERT INTO users (id, code, email, is_moderator, name, password, photo, reg_time)
+VALUES ( 1,
+        'userUser',
+        '9824104901@mail.ru',
+        0,
+        'User',
+        '$2y$12$bTfQ.I1qLk0Z4dLdc3nylOzfwchQPkqKdCWXk6WFhYLDppCsYfS7S',
+        'us',
+        '2021-04-10');
+
+INSERT INTO users (id, code, email, is_moderator, name, password, photo, reg_time)
+VALUES (2,
+        'modeMode',
+        'anmiha0672@mail.ru',
+        1,
+        'Moderator',
+        '$2y$12$bTfQ.I1qLk0Z4dLdc3nylOzfwchQPkqKdCWXk6WFhYLDppCsYfS7S',
+        'us',
+        '2021-04-10');
--- target/classes/db/migration/V1__create_bloc_basa.sql
+++ target/classes/db/migration/V1__create_bloc_basa.sql
@@ -1,20 +1,32 @@
+
 create table captcha_codes (
                                id integer not null auto_increment,
                                code varchar(255) not null,
                                secret_code varchar(255) not null,
                                time datetime(6) not null,
                                primary key (id)
-) engine=InnoDB
-Hibernate:
+) engine=InnoDB;
+
 
 create table global_settings (
-                                 id integer not null auto_increment,
-                                 code varchar(255),
-                                 name varchar(255),
-                                 value bit,
+                                 id integer not null,
+                                 code varchar(255) not null,
+                                 name varchar(255) not null,
+                                 value varchar(255) not null,
                                  primary key (id)
-) engine=InnoDB
-Hibernate:
+) engine=InnoDB;
+
+
+create table hibernate_sequence (
+    next_val bigint
+) engine=InnoDB;
+
+
+insert into hibernate_sequence values ( 1 );
+
+
+insert into hibernate_sequence values ( 1 );
+
 
 create table post_comments (
                                id integer not null auto_increment,
@@ -24,8 +36,7 @@ create table post_comments (
                                post_id integer not null,
                                user_id integer not null,
                                primary key (id)
-) engine=InnoDB
-Hibernate:
+) engine=InnoDB;
 
 create table post_votes (
                             id integer not null auto_increment,
@@ -34,8 +45,8 @@ create table post_votes (
                             value tinyint not null,
                             post_id integer not null,
                             primary key (id)
-) engine=InnoDB
-Hibernate:
+) engine=InnoDB;
+
 
 create table posts (
                        id integer not null auto_increment,
@@ -48,68 +59,73 @@ create table posts (
                        view_count integer not null,
                        user_id integer not null,
                        primary key (id)
-) engine=InnoDB
-Hibernate:
+) engine=InnoDB;
+
 
 create table tag2post (
                           id integer not null auto_increment,
                           post_id integer not null,
                           tag_id integer not null,
                           primary key (id)
-) engine=InnoDB
-Hibernate:
+) engine=InnoDB;
+
 
 create table tags (
                       id integer not null auto_increment,
                       name varchar(255) not null,
                       primary key (id)
-) engine=InnoDB
-Hibernate:
+) engine=InnoDB;
+
 
 create table users (
                        id integer not null auto_increment,
                        code varchar(255),
                        email varchar(255) not null,
-                       is_moderator tinyint not null,
+                       is_moderator TINYINT not null,
                        name varchar(255) not null,
                        password varchar(255) not null,
-                       photo varchar(255),
-                       reg_time datetime(6) not null,
+                       photo Text,
+                       reg_time DATETIME not null,
                        primary key (id)
-) engine=InnoDB
-Hibernate:
+) engine=InnoDB;
+
 
 alter table post_comments
     add constraint FKaawaqxjs3br8dw5v90w7uu514
         foreign key (post_id)
-            references posts (id)
-    Hibernate:
+            references posts (id);
+
 
 alter table post_comments
     add constraint FKsnxoecngu89u3fh4wdrgf0f2g
         foreign key (user_id)
-            references users (id)
-    Hibernate:
+            references users (id);
+
 
 alter table post_votes
     add constraint FK9jh5u17tmu1g7xnlxa77ilo3u
         foreign key (post_id)
-            references posts (id)
-    Hibernate:
+            references posts (id);
+
 
 alter table posts
     add constraint FK5lidm6cqbc7u4xhqpxm898qme
         foreign key (user_id)
-            references users (id)
-    Hibernate:
+            references users (id);
+
 
 alter table tag2post
     add constraint FKjou6suf2w810t2u3l96uasw3r
         foreign key (tag_id)
-            references tags (id)
-    Hibernate:
+            references tags (id);
+
 
 alter table tag2post
     add constraint FKpjoedhh4h917xf25el3odq20i
         foreign key (post_id)
-            references posts (id)
+            references posts (id);
+
+
+
+
+
